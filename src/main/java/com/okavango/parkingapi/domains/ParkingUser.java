66696a512a36dc39535edc0a5ccc64baacffcae6
okavango81 <eguilhermeleitedev81@gmail.com>
2024-02-21 08:14:59 -0300

    entity ParkingUser

---
+++ src/main/java/com/okavango/parkingapi/domains/ParkingUser.java
@@ -0,0 +1,70 @@
+package com.okavango.parkingapi.domains;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class ParkingUser implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(nullable = false, length = 200)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 25)
+    private Role role;
+
+    private LocalDateTime creationDate;
+    private LocalDateTime modificationDate;
+    private String createdBy;
+    private String modifiedBy;
+
+    public enum Role{
+        ROLE_ADMIN, ROLE_CLIENT
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ParkingUser that = (ParkingUser) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "ParkingUser{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                ", creationDate=" + creationDate +
+                ", modificationDate=" + modificationDate +
+                ", createdBy='" + createdBy + '\'' +
+                ", modifiedBy='" + modifiedBy + '\'' +
+                '}';
+    }
+}
