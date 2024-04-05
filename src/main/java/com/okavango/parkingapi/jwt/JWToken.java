1a7e070c1800548f220d8eba009cbd0e51b973a6
okavango81 <eguilhermeleitedev81@gmail.com>
2024-04-05 16:07:07 -0300

    created class JWToken

---
+++ src/main/java/com/okavango/parkingapi/jwt/JWToken.java
@@ -0,0 +1,15 @@
+package com.okavango.parkingapi.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class JWToken {
+
+    private String token;
+}
