681e24f33b4f64118e194e00f70befa81067f3d0
okavango81 <eguilhermeleitedev81@gmail.com>
2024-04-09 14:55:21 -0300

    class JWTAuthorizationFilter

---
+++ src/main/java/com/okavango/parkingapi/jwt/JWTAuthorizationFilter.java
@@ -0,0 +1,50 @@
+package com.okavango.parkingapi.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JWTUserDetailService userDetailService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String token = request.getHeader(JWTUtils.JWT_AUTHORIZATION);
+
+        if(token == null || !token.startsWith(JWTUtils.JWT_BEARER)){
+            log.info("JWT token is null, empty or not inicialized with Bearer");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if(!JWTUtils.isTokenValid(token)){
+            log.warn("JWT token is not valid or expided");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String username = JWTUtils.getUserNameFromToke(token);
+        toAuthentication(request, username);
+        filterChain.doFilter(request, response);
+    }
+
+    private void toAuthentication(HttpServletRequest request, String username) {
+        UserDetails details = userDetailService.loadUserByUsername(username);
+        UsernamePasswordAuthenticationToken authenticationToken = UsernamePasswordAuthenticationToken.authenticated(details, null , details.getAuthorities());
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    }
+}
