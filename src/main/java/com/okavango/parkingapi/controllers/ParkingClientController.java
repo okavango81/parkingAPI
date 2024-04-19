ecb19511f81d0ceeed0e9af2e01a8126ff9ffdc7
okavango81 <eguilhermeleitedev81@gmail.com>
2024-04-18 23:10:48 -0300

    documentation resources to ParkingClient

--- src/main/java/com/okavango/parkingapi/controllers/ParkingClientController.java
+++ src/main/java/com/okavango/parkingapi/controllers/ParkingClientController.java
@@ -1,15 +1,18 @@
 package com.okavango.parkingapi.controllers;
 
 
+import com.okavango.parkingapi.docs.parking_user.SwaggerParkingClient;
 import com.okavango.parkingapi.domains.dtos.ParkingClientDTO;
 import com.okavango.parkingapi.domains.dtos.ParkingClientMinDTO;
 import com.okavango.parkingapi.services.ParkingClientService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "ParkingClients", description = "Operations related to the ParkingClient entity")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/clients")
@@ -19,12 +22,14 @@ public class ParkingClientController {
 
     @PostMapping
     @PreAuthorize("hasRole('CLIENT')")
+    @SwaggerParkingClient.NewClient
     public ResponseEntity<ParkingClientMinDTO> register(@Valid @RequestBody ParkingClientDTO client){
         return parkingClientService.registration(client);
     }
 
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
+    @SwaggerParkingClient.ClientById
     public ResponseEntity<ParkingClientMinDTO> findById(@PathVariable Long id){
         return parkingClientService.findId(id);
     }
---
+++ src/main/java/com/okavango/parkingapi/docs/parking_user/SwaggerParkingClient.java
@@ -0,0 +1,63 @@
+package com.okavango.parkingapi.docs.parking_user;
+
+import com.okavango.parkingapi.domains.dtos.ParkingClientMinDTO;
+import com.okavango.parkingapi.domains.dtos.ParkingUserMinDTO;
+import com.okavango.parkingapi.exceptions.ExceptionObject;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public class SwaggerParkingClient {
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.METHOD)
+    @Operation(summary = "Register", description = "Register a new parking client (Operation permited only to CLIENT profile authenticated)",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Resource created successfully",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ParkingClientMinDTO.class))
+                    ),
+                    @ApiResponse(responseCode = "401", description = "User unauthorized"),
+                    @ApiResponse(responseCode = "403", description = "User have be not permission to access this resource",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionObject.class))
+                    )
+            }
+    )
+    public @interface NewClient{}
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.METHOD)
+    @Operation(summary = "Return by id", description = "Return parking client by id (Operation permited only to ADMIN profile authenticated)",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "401", description = "User unauthorized"),
+                    @ApiResponse(responseCode = "200", description = "Return of parking client defined by id",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ParkingUserMinDTO.class))
+                    ),
+                    @ApiResponse(responseCode = "404", description = "Resource not found / The URI does not contain a valid identifier",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionObject.class))
+                    ),
+
+                    @ApiResponse(responseCode = "400", description = "Check the full URL as there may be typos",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionObject.class))
+                    ),
+                    @ApiResponse(responseCode = "403", description = "User have be not permission to access this resource",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionObject.class))
+                    )
+            }
+    )
+    public @interface ClientById {}
+}
