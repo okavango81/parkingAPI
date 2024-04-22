a11abd391a0be0c7f65db66c078b95fc7f6f1f5b
okavango81 <eguilhermeleitedev81@gmail.com>
2024-04-22 00:00:31 -0300

    save and return location to ParkingSpace (refact)

--- src/main/java/com/okavango/parkingapi/repositories/ParkingSpaceRepository.java
+++ src/main/java/com/okavango/parkingapi/repositories/ParkingSpaceRepository.java
@@ -1,7 +1,10 @@
 package com.okavango.parkingapi.repositories;
 
 import com.okavango.parkingapi.domains.ParkingSpace;
+import com.okavango.parkingapi.domains.dtos.ParkingSpaceMinDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 
 public interface ParkingSpaceRepository extends JpaRepository<ParkingSpace, Long> {
+    ParkingSpace findByCode(String code);
 }
